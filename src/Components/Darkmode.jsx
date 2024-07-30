ae5ac92c82a4b8f914b1dd9760289959a7ee0212
unknown <njerivictory52@gmail.com>
2024-07-30 16:42:21 +0300

    add transition

--- src/Components/Darkmode.jsx
+++ src/Components/Darkmode.jsx
@@ -7,10 +7,11 @@ import './DarkMode.css'
 function Darkmode() {
     const setDarkMode=()=>{
         document.querySelector("body").setAttribute("data-theme","dark")
+        localStorage.setItem("selectedTheme","dark")
     }
     const setLightTheme=()=>{
         document.querySelector("body").setAttribute("data-theme","light")
-
+localStorage.setItem("selectedTheme","light")
     }
   const toggleTheme=(e)=>{
 if (e.target.checked)setDarkMode();
